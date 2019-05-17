c7042219eabecac8aeef1ff40b0622224001e32e
Dmytro <pankovdm@gmail.com>
2019-05-17 14:39:41 -0700

    ProfilePage

--- app/components/pages/ProfilePage/ProfilePage.js
+++ app/components/pages/ProfilePage/ProfilePage.js
@@ -4,11 +4,13 @@ import { Helmet } from 'react-helmet';
 import Header from '../../containers/Header';
 import Input from '../../elements/Input';
 import './style.scss';
+import Button from '../../elements/Button';
+import Footer from '../../containers/Footer/Footer';
 
 export default class ProfilePage extends Component {
   render() {
     return (
-      <div className="feature-page">
+      <div className='ProfilePageWrapper'>
         <Helmet>
           <title>Profile Page</title>
           <meta
@@ -17,20 +19,55 @@ export default class ProfilePage extends Component {
           />
         </Helmet>
         <Header />
-        <h2>General Information</h2>
-        <Input />
-        <Input />
-        <Input />
-        <h2>Contact Information</h2>
-        <Input />
-        <Input />
-        <Input />
-        <h2>Change Password</h2>
-        <Input />
-        <Input />
-        <Input />
-          This is Profile Page
+
+        <div className='SubHeaderWrapper'>
+          <h3 className='SubHeaderH3'>Personal account</h3>
+          <div className='BtnWrapper'>
+            <Button text={'+ Create an ad'} active={'active'} />
+          </div>
+        </div>
+
+
+          <div className='ProfilePageSideBar'>
+
+            <div className='PersonalInfoWrapper'>
+              <img/>
+              <h5>James Bond</h5>
+              <p>Geroev Stalingrada 15</p>
+              <p>+38 000 000 00 00</p>
+              <div>Rating</div>
+
+            </div>
+            <h5>My profile</h5>
+            <h5>My shared stuff</h5>
+            <Button text={'+ Create an ad'} active={'active'} />
+            <Footer />c
+          </div>
+
+          <div className='ProfileInputWrapper'>
+            <div>
+              <h2>General Information</h2>
+              <Input />
+              <Input />
+              <Input />
+            </div>
+            <div>
+              <h2>Contact Information</h2>
+              <Input />
+              
+              <Input />
+            </div>
+            <div>
+              <h2>Change Password</h2>
+              <Input />
+              <Input />
+              <Input />
+            </div>
+            <Button text={'Safe'} active={'active'} />
+          </div>
+
       </div>
     );
   }
 }
+
