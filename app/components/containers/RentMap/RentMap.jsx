daf5f7789d2c0d1d9940b7dbbc340b1bf047b51f
Olga-Garanko <olga.garanko@gmail.com>
2019-06-23 23:22:21 +0300

    api

--- app/components/containers/RentMap/RentMap.jsx
+++ app/components/containers/RentMap/RentMap.jsx
@@ -3,12 +3,9 @@ import PropTypes from 'prop-types';
 import {
   Map as LeafletMap, TileLayer, Marker, Popup
 } from 'react-leaflet';
-import ItemList from './mock/ItemListFile.JSON';
-const itemList = ItemList.list;
-console.log(itemList)
-const RentMap = ({ styles }) => (
+const RentMap = ({ styles, lots }) => (
       <LeafletMap
-        center={[itemList[0].coordinates.latitude, itemList[0].coordinates.longitude]}
+        center={[901.9, 801.8]}
         zoom={14}
         maxZoom={20}
         attributionControl
@@ -22,8 +19,7 @@ const RentMap = ({ styles }) => (
         <TileLayer
           url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
         />
-        {Object.values}
-        {itemList.map((item) => (
+        {lots.map((item) => (
           <Marker key={item.id} position={[item.coordinates.latitude, item.coordinates.longitude]}>
             <Popup>
               {item.description.lotName}
