92da385620b44d12ae7085ba1effa4ff3c4fae1b
Vitalii <vitalii@Vitaliis-MacBook-Air.local>
2019-07-02 23:44:46 +0300

    Fix bug with duble click on page for load lots

--- app/components/pages/HomePage/HomePage.jsx
+++ app/components/pages/HomePage/HomePage.jsx
@@ -2,61 +2,68 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import LotsList from 'components/containers/LotsList';
 import Footer from 'components/containers/Footer';
-import RentMap from 'components//containers/RentMap';
+import RentMap from 'components/containers/RentMap';
 import Pagination from 'components/containers/Pagination';
-import { Helmet } from 'react-helmet';
-
+/* import { baseUrl, getData} from 'utils/api.js'; */
+const baseURL = 'http://ec2-52-206-69-68.compute-1.amazonaws.com:8080/lot/';
 class HomePage extends Component {
   static propTypes = {
     styles: PropTypes.object.isRequired
   }
 
   state = {
-    items: [],
-    currentPage: 1,
-    itemsPerPage: 15,
-    pagesList: 5
+    itemsPerPage: 3,
+    currentPageLots: {
+      pageNumber: 1,
+      lots: [],
+      totalPages: 0
+    },
+    lotsAll: [],
   };
 
   componentDidMount = () => {
-    const { currentPage } = this.state;
-    this.getLots(currentPage);
+    this.getData(this.getPageUrl(), 'currentPageLots');
+    this.getData(baseURL, 'lotsAll');
+  }
+
+  getPageUrl = () => {
+    const { currentPageLots: { pageNumber }, itemsPerPage } = this.state;
+    return `${baseURL}page/${pageNumber}/${itemsPerPage}/current`;
+  }
+
+  getData = (url, target) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          [target]: data
+        });
+      });
   }
 
   setCurrentPage = (currentPage) => {
-    console.log(this.state);
-    this.setState({ currentPage });
-    this.getLots(currentPage);
-  }
-
-  getLots = (numberPage) => {
-    const { itemsPerPage } = this.state;
-    fetch(`http://ec2-54-173-110-187.compute-1.amazonaws.com:8080/lot/page/${numberPage}/${itemsPerPage}/first`)
-      .then((res) => res.json())
-      .then((res) => {
-        this.setState({
-          items: res.content
-        });
-      });
+    const { currentPageLots } = this.state;
+    currentPageLots.pageNumber = currentPage;
+    this.setState({ currentPageLots });
+    this.getData(this.getPageUrl(), 'currentPageLots');
   }
 
   render() {
     const { styles } = this.props;
     const {
-      items,
-      currentPage,
-      pagesList
+      currentPageLots: { pageNumber, totalPages, lots },
+      lotsAll
     } = this.state;
     return (
       <div>
         <div className={styles.content}>
-          <LotsList itemsList={items} />
+          <LotsList lots={lots} />
           <div>
-            <RentMap />
+            <RentMap lots={lotsAll} />
           </div>
         </div>
         <div>
-          <Pagination getCurrentPage={this.setCurrentPage} currentPage={currentPage} pagesList={pagesList} />
+          <Pagination getCurrentPage={this.setCurrentPage} currentPage={pageNumber} pagesList={totalPages} />
         </div>
         <Footer />
       </div>
