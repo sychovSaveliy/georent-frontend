d0c64f69972335d81c6330d3a9462ab48f97c2fa
nick <nick.@avalr.com.ua>
2019-08-02 21:49:23 +0300

    georentFront-update-pass  - add App.js

--- app/App/App.jsx
+++ app/App/App.jsx
@@ -18,6 +18,8 @@ import DetailsPage from 'pages/DetailsPage';
 import UserLotsPage from 'pages/UserLotsPage';
 import ForgotPassPage from 'pages/ForgotPassPage';
 
+import queryString from 'query-string'
+
 export default class App extends Component {
   static propTypes = {
     styles: PropTypes.object.isRequired
@@ -28,46 +30,82 @@ export default class App extends Component {
       isLogged: window.localStorage.getItem("jwt") ? 1 : 0,
     };
   };
+
+  
   login = () => {
-      this.setState({
-        isLogged : true
-      });
+    this.setState({
+      isLogged: true
+    });
   };
   exit = () => {
-      window.localStorage.removeItem("jwt")
-      this.setState({
-        isLogged : false
-      });
+    window.localStorage.removeItem("jwt")
+    this.setState({
+      isLogged: false
+    });
   };
   render() {
     const { styles } = this.props;
     const { isLogged } = this.state;
+    const valuesHref = queryString.parse(window.location.href);
+    const pathForgot = valuesHref.path + "";
+    const tokenType = valuesHref.tokentype + "";
+    const accessToken = valuesHref.accesstoken;
+
+
+    // const NoMatchPage = () => {  return (    <h3>404 - Not found</h3>  );};
+    // const App = () => {
+    //   return (
+    //     <section className="App">
+    //       <Router>
+    //         <Link to="/">Home</Link>
+    //         {/* <Link to="/users">Users</Link> */}
+    //         <Link to="/forgot?q=react">Search</Link>
+
+    //         <Route path="/" component={IndexPage} />
+    //         {/* <Route exact path="/about" component={AboutPage} /> */}
+    //         <Route exact path="/forgot" component={ForgotPassPage} />
+    //         <Route component={NoMatchPage} />      </Router>
+    //     </section>
+    //   );
+    // };
+    
+
+
+    debugger
     return (
       <div className={styles.appWrapper}>
         <Helmet titleTemplate="%s" defaultTitle="Geo Rent">
           <meta name="description" content="Geo Rent" />
         </Helmet>
         <Header isLogged={isLogged} onExit={this.exit} />
+
         <Switch>
-          <Route exact path="/" render={props => {return <HomePage {...props} isLogged={isLogged} onExit={this.exit} />}} />
-          <Route exact path="/lots" render={props => {return <HomePage {...props} isLogged={isLogged} onExit={this.exit} />}} />
-          <Route exact path="/user/lot/:lotId" render={props => {return <DetailsPage {...props} isLogged={isLogged} onExit={this.exit} />}} />
-          <Route path="/features" render={props => {return <FeaturePage {...props} isLogged={isLogged} onExit={this.exit} />}} />
-          <Route path="/signup" render={props => {return <RegistrationPage {...props} isLogged={isLogged} />}} />
-          <Route path="/forgot" render={props => {return <ForgotPassPage {...props} isLogged={isLogged} />}} />
-          <Route path="/login" render={props => {return <LoginPage {...props} isLogged={isLogged} onLogin={this.login} />}} />
+          <Route exact path="/" render={props => { 
+            if ((pathForgot === "forgot") && (tokenType === "Bearer") && accessToken)   {
+              // <Redirect from="/" to="/forgot" />
+             return <ForgotPassPage {...props} isLogged={isLogged} />
+            }
+            return <HomePage {...props} isLogged={isLogged} onExit={this.exit} /> 
+            
+            }} />
+          <Route exact path="/lots" render={props => { return <HomePage {...props} isLogged={isLogged} onExit={this.exit} /> }} />
+          <Route exact path="/user/lot/:lotId" render={props => { return <DetailsPage {...props} isLogged={isLogged} onExit={this.exit} /> }} />
+          <Route path="/features" render={props => { return <FeaturePage {...props} isLogged={isLogged} onExit={this.exit} /> }} />
+          <Route path="/signup" render={props => { return <RegistrationPage {...props} isLogged={isLogged} /> }} />
+          {/* <Route path="/forgot" render={props => {return <ForgotPassPage {...props} isLogged={isLogged} />}} /> */}
+          <Route path="/login" render={props => { return <LoginPage {...props} isLogged={isLogged} onLogin={this.login} /> }} />
           <PrivateRoute exact path="/profile" component={ProfilePage} isLogged={isLogged} onExit={this.exit} />
           <PrivateRoute path="/profile/edit" component={ProfilePageEdit} isLogged={isLogged} onExit={this.exit} />
           <Route path="/create-ad" render={props => {
-              {/*if (!window.localStorage.getItem("jwt")) {
+            {/*if (!window.localStorage.getItem("jwt")) {
                   return <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
               } else */}
-              return <CreateAdPage {...props} isLogged={isLogged} onExit={this.exit} />
+            return <CreateAdPage {...props} isLogged={isLogged} onExit={this.exit} />
           }} />
           <Route path="/user/lots" render={props => {
-              return <UserLotsPage {...props} isLogged={isLogged} onExit={this.exit} />
+            return <UserLotsPage {...props} isLogged={isLogged} onExit={this.exit} />
           }} />
-          <Route path="*" render={props => {return <NotFoundPage {...props} isLogged={isLogged} />}} />
+          <Route path="*" render={props => { return <NotFoundPage {...props} isLogged={isLogged} /> }} />
         </Switch>
         <Footer isLogged={isLogged} />
       </div>
